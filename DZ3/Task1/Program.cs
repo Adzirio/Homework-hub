6af6286b1b39ed6238f4b624fcd8441ab476cf93
DenisZyryanov <dziro744@gmail.com>
2021-01-23 23:09:02 +0700

    Completed
    Переделан 1й проект, переименованы проекты, правки в 4м.

--- DZ3/Task1/Program.cs
+++ DZ3/Task1/Program.cs
@@ -7,16 +7,31 @@ namespace Task1
         static void Main(string[] args)
         {
             Console.WriteLine("Диагональ.");
-            Console.Write("Введите желаемое слово: ");
-            string userWord = Console.ReadLine();
-            string n = "";
-            for (int i = 0; i < userWord.Length; i++)
+            Console.Write("Введите желаемую длинну сторон двухмерного массива: ");
+            string n = Console.ReadLine();
+            int lenght = Convert.ToInt32(n);
+            int[,] nArray = new int[lenght, lenght];
+            int num = 0;
+            for (int i = 0; i < nArray.GetLength(0); i++)
             {
-                Console.Write(n + userWord[i]);
-                Console.WriteLine();
-                n += "  ";
+                for (int j = 0; j < nArray.GetLength(0); j++)
+                {
+                    nArray[i, j] = num;
+                    num++;
+                }
             }
-
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write(nArray[i, i]);
+                if (i <  lenght - 1)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
         }
     }
 }
