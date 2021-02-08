f561b06c5699a25cb8e2cdd2322f7d83f99bf346
DenisZyryanov <dziro744@gmail.com>
2021-02-08 19:27:35 +0700

    Сделано DZ6.

---
+++ DZ6/Task Manager/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessList();
+            while (true)
+            {
+                Console.WriteLine("\nДля выхода пропишите: х");
+                Console.Write("Введите ID процесса для его завершения: ");
+                string id = Console.ReadLine();
+                if (id == "x" || id == "х")
+                {
+                    break;
+                }
+                try
+                {
+                    using (Process program = Process.GetProcessById(int.Parse(id)))
+                    {
+                        program.Kill();
+                        program.WaitForExit();
+                        Console.Clear();
+                        ProcessList();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\n!!!Неверное значение!!!");
+                    continue;
+                }
+            }
+        }
+        static void ProcessList()
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                Console.WriteLine($"Name: ({process.Id}) {process.ProcessName}");
+            }
+        }
+    }
+}
