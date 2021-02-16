415b3c7714f2a712cc8b33b54d3a30d1404208cb
DenisZyryanov <dziro744@gmail.com>
2021-02-16 17:37:52 +0700

    Сделано DZ8.

---
+++ DZ8/Mask1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Properties.Settings.Default.UserName;
+            string greeting = Properties.Settings.Default.Greeting;
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName) || string.IsNullOrEmpty(Properties.Settings.Default.UserAge) || string.IsNullOrEmpty(Properties.Settings.Default.UserActivity))
+            {
+                Console.Write("Не зарегистрированный пользователь.\nВведите запрашиваемые данные.\nИмя: ");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Console.Write("Возраст: ");
+                Properties.Settings.Default.UserAge = Console.ReadLine();
+                Console.Write("Вид деятельности: ");
+                Properties.Settings.Default.UserActivity = Console.ReadLine();
+
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine($"{greeting}, {userName}");
+            }
+            Console.Write("\nДля выхода нажмите ENTER.");
+            Console.ReadLine();
+        }
+    }
+}
