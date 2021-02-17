35cf426ebaf257b5f5adcb5cbd4241d3b7a8fc5f
DenisZyryanov <dziro744@gmail.com>
2021-02-17 14:02:18 +0700

    Сделано DZ1.

---
+++ Алгоритмы и структуры данных/DZ1/Mask1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;  //Не простое
+                        //Простыми являются числа 1,2,3
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+            string answer = "";
+            if (d == 0)
+            {
+                answer = "Простое";
+            }
+            else
+            {
+                answer = "Не простое";
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Алгоритмы и структуры данных/DZ1/Mask3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Mask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 12;
+            Recursive(n);    //144
+            int N = 10;
+            NoRecursive(N);  //55
+        }
+        static int Recursive(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            return Recursive(n - 2) + Recursive(n - 1);
+        }
+        static int NoRecursive(int n)
+        {
+            int even = 0;
+            int notEven = 1;
+            for(int i = 1; i < n; i++)
+            {
+                if (even < notEven)
+                {
+                    even += notEven;
+                }
+                else
+                {
+                    notEven += even;
+                }
+            }
+            if (even > notEven || n == 0) 
+            {
+                return even;
+            }
+            return notEven;
+        }
+    }
+}
